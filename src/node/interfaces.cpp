9744a7a8ab0902d12514685da36543b5c9c1c167
zaidmstrr <zaidbrock122@gmail.com>
2025-05-20 17:39:41 +0530

    rpc: add new removetxfrommempool RPC and IPC interface

--- src/node/interfaces.cpp
+++ src/node/interfaces.cpp
@@ -553,6 +553,19 @@ public:
         LOCK(::cs_main);
         return Assert(chainman().ActiveChain()[height])->GetBlockHash();
     }
+    bool removeTxFromMempool(const uint256& txid) override
+    {
+        NodeContext& node = m_node;
+        if (!node.mempool) return false;
+        CTxMemPool& mempool = *node.mempool;
+        LOCK(mempool.cs);
+        CTransactionRef tx = mempool.get(txid);
+        if (tx) {
+            mempool.removeRecursive(*tx, MemPoolRemovalReason::MANUAL);
+            return true;
+        }
+        return false;
+    }
     bool haveBlockOnDisk(int height) override
     {
         LOCK(::cs_main);
