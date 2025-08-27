ccb33355ee2ce399d538e0457930e56984bc9020
Bob McElrath <mcelrath@users.noreply.github.com>
2025-08-27 15:42:34 -0400

    CPUNet Test Network
        See documentation in contrib/cpunet/README.md
    
    Revert debug logging in zmq_sub.py
    
    Recommend -ntasks=1 for cpunet
    
        We don't want a ton of hashrate, one core per node is enough.
    
    Call bitcoin-cli asynchronously; docs and commenting
    
    Add ZMQ connection monitoring
    
    Updating the CPUnet commits upto the cmempool branch
    
    Work around ZMQ/getblocktemplate race condition in bitcoind

--- src/bitcoin-wallet.cpp
+++ src/bitcoin-wallet.cpp
@@ -62,12 +62,11 @@ static std::optional<int> WalletAppInit(ArgsManager& args, int argc, char* argv[
             strUsage += FormatParagraph(LicenseInfo());
         } else {
             strUsage += "\n"
-                "bitcoin-wallet is an offline tool for creating and interacting with " CLIENT_NAME " wallet files.\n\n"
-                "By default bitcoin-wallet will act on wallets in the default mainnet wallet directory in the datadir.\n\n"
-                "To change the target wallet, use the -datadir, -wallet and (test)chain selection arguments.\n"
-                "\n"
-                "Usage: bitcoin-wallet [options] <command>\n"
-                "\n";
+                        "bitcoin-wallet is an offline tool for creating and interacting with " CLIENT_NAME " wallet files.\n"
+                        "By default bitcoin-wallet will act on wallets in the default mainnet wallet directory in the datadir.\n"
+                        "To change the target wallet, use the -datadir, -wallet and -regtest/-signet/-testnet/-testnet4/-cpunet arguments.\n\n"
+                        "Usage:\n"
+                        "  bitcoin-wallet [options] <command>\n";
             strUsage += "\n" + args.GetHelpMessage();
         }
         tfm::format(std::cout, "%s", strUsage);
